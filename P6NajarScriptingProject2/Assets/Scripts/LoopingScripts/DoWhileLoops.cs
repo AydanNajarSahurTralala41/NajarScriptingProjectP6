d3a3c88bc3e1215541c4ed2813780da54d06116f
AydanNajarSahurTralala41 <726038@student.dusd.net>
2026-01-06 12:27:47 -0800

    finished some scripts
    finished my scripts in unity

---
+++ P6NajarScriptingProject2/Assets/Scripts/LoopingScripts/DoWhileLoops.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+ 
+
+public class DoWhileLoop : MonoBehaviour
+{
+    
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print ("The new meme is 6.......7");
+
+
+        }while(shouldContinue == true);
+    }
+}
+    
